f60dfe2568b378e0a720d1501361b475f265e2ce
pauli_iyaji <p3.iyaji@gmail.com>
2024-10-05 06:41:37 +0100

    soccer world edited to create a pull request

--- resources/views/livewire/soccerworld.blade.php
+++ resources/views/livewire/soccerworld.blade.php
@@ -3,5 +3,5 @@
    <h2 class="text-2xl text-indigo-600 font-semibold">Somethings like to joke joor</h2>
     <h4 class="text-4xl text-amber-800">This is also a test</h4>
     <p class="text-indigo-600 font-semibold">It is coming out well</p>
-
+    <p class="text-amber-800">Just testing pull request from phpstorm</p>
 </div>

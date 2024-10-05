8ba55940769689603cc17eab00f9ef00b9894c67
pauli_iyaji <p3.iyaji@gmail.com>
2024-10-05 06:34:07 +0100

    soccer world edited

--- resources/views/livewire/soccerworld.blade.php
+++ resources/views/livewire/soccerworld.blade.php
@@ -1,5 +1,6 @@
 <div class="flex items-center justify-center mt-20 md:w-full md:mx-auto">
 
    <h2 class="text-2xl text-indigo-600 font-semibold">Somethings like to joke joor</h2>
+    <h4 class="text-4xl text-amber-800">This is also a test</h4>
 
 </div>
